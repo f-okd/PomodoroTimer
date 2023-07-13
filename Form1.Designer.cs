9362425afc2d0ba045c17bf05174623d9061b510
faye <oluwafayefunmiokude@gmail.com>
2023-07-13 02:23:35 +0100

    focus timer completed, need to add enums to switch between break/focus timer also stop timer should trigger end of pomo to differentiate from start timer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,11 @@
             components = new System.ComponentModel.Container();
             btn_start = new Button();
             label_timer = new Label();
-            timer_countdown = new System.Windows.Forms.Timer(components);
+            timer_focus = new System.Windows.Forms.Timer(components);
+            btn_pause = new Button();
+            btn_continue = new Button();
+            btn_stop = new Button();
+            timer_break = new System.Windows.Forms.Timer(components);
             SuspendLayout();
             // 
             // btn_start
@@ -53,16 +57,53 @@
             label_timer.TabIndex = 1;
             label_timer.Text = "label1";
             // 
-            // timer_countdown
+            // timer_focus
             // 
-            timer_countdown.Interval = 1000;
-            timer_countdown.Tick += timer_countdown_Tick;
+            timer_focus.Interval = 1000;
+            timer_focus.Tick += timer_focus_Tick;
+            // 
+            // btn_pause
+            // 
+            btn_pause.Location = new Point(315, 312);
+            btn_pause.Name = "btn_pause";
+            btn_pause.Size = new Size(161, 45);
+            btn_pause.TabIndex = 2;
+            btn_pause.Text = "Pause";
+            btn_pause.UseVisualStyleBackColor = true;
+            btn_pause.Click += btn_pause_Click;
+            // 
+            // btn_continue
+            // 
+            btn_continue.Location = new Point(213, 248);
+            btn_continue.Name = "btn_continue";
+            btn_continue.Size = new Size(161, 45);
+            btn_continue.TabIndex = 3;
+            btn_continue.Text = "Continue";
+            btn_continue.UseVisualStyleBackColor = true;
+            btn_continue.Click += btn_continue_Click;
+            // 
+            // btn_stop
+            // 
+            btn_stop.Location = new Point(413, 248);
+            btn_stop.Name = "btn_stop";
+            btn_stop.Size = new Size(161, 45);
+            btn_stop.TabIndex = 4;
+            btn_stop.Text = "Stop";
+            btn_stop.UseVisualStyleBackColor = true;
+            btn_stop.Click += btn_stop_Click;
+            // 
+            // timer_break
+            // 
+            timer_break.Interval = 1000;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btn_stop);
+            Controls.Add(btn_continue);
+            Controls.Add(btn_pause);
             Controls.Add(label_timer);
             Controls.Add(btn_start);
             Name = "Form1";
@@ -75,6 +116,10 @@
 
         private Button btn_start;
         private Label label_timer;
-        private System.Windows.Forms.Timer timer_countdown;
+        private System.Windows.Forms.Timer timer_focus;
+        private Button btn_pause;
+        private Button btn_continue;
+        private Button btn_stop;
+        private System.Windows.Forms.Timer timer_break;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ namespace PomodoroTimer
             InitializeComponent();
             // Set to value of pomodoro length in form 2
             label_timer.Text = string.Empty;
+            btn_continue.Visible = false;
+            btn_stop.Visible = false;
+            btn_pause.Visible = false;
         }
 
         static int pomodoro_length = 25 * 60;
@@ -16,22 +19,61 @@ namespace PomodoroTimer
         {
             // Set pomodoro timer to 25 minutes; TODO: Take input from form 2
 
-            timer_countdown.Start();
+            restartTimer();
+            btn_start.Visible = false;
+            btn_pause.Visible = true;
         }
 
-        private void timer_countdown_Tick(object sender, EventArgs e)
+        private void displayTime(int seconds)
+        {
+            TimeSpan time_left = TimeSpan.FromSeconds(time_left_secs);
+            label_timer.Text = time_left.ToString("mm\\:ss");
+        }
+        private void timer_focus_Tick(object sender, EventArgs e)
         {
             if (time_left_secs > 0)
             {
                 time_left_secs -= 1;
-                TimeSpan time_left = TimeSpan.FromSeconds(time_left_secs);
-                label_timer.Text = time_left.ToString("mm\\:ss");
+                displayTime(time_left_secs);
             }
             else
             {
-                timer_countdown.Stop();
-                label_timer.Text = "Time Up!!";
+                timer_focus.Stop();
+                restartTimer();
+                label_timer.Text = "Time Up!!"; // add alarm sounds
             }
+        }
+
+        private void restartTimer()
+        {
+            time_left_secs = pomodoro_length;
+            displayTime(time_left_secs);
+            timer_focus.Start();
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            timer_focus.Stop();
+            btn_pause.Visible = false;
+            btn_continue.Visible = true;
+            btn_stop.Visible = true;
+        }
+
+        private void btn_continue_Click(object sender, EventArgs e)
+        {
+            timer_focus.Start();
+            btn_continue.Visible = false;
+            btn_stop.Visible = false;
+            btn_pause.Visible = true;
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            restartTimer();
+            timer_focus.Stop();
+            btn_stop.Visible = false;
+            btn_continue.Visible = false;
+            btn_start.Visible = true;
         }
     }
 }
